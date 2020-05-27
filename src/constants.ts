e44490bcc11147d27344d03248acf4641edc4bc7
Kamran Ayub <kamran.ayub@gmail.com>
2020-05-27 15:54:08 -0500

    wip add types

---
+++ src/constants.ts
@@ -0,0 +1,23 @@
+import { ReflectionKind } from 'typedoc/dist/lib/models/reflections/abstract'
+
+export const SYMBOL_CONTAINERS = [
+  ReflectionKind.Class,
+  ReflectionKind.Interface,
+  ReflectionKind.Enum,
+  ReflectionKind.Module,
+  ReflectionKind.SomeModule,
+]
+
+export const SYMBOL_LINK_KINDS = [
+  ReflectionKind.Enum,
+  ReflectionKind.EnumMember,
+  ReflectionKind.Class,
+  ReflectionKind.Interface,
+  ReflectionKind.Constructor,
+  ReflectionKind.Property,
+  ReflectionKind.Method,
+  ReflectionKind.Accessor,
+  ReflectionKind.Function,
+  ReflectionKind.TypeAlias,
+  ReflectionKind.ObjectLiteral,
+]
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,94 @@
-export const greet = (name: string) => `Hello ${name}`
+import modifyChildren from 'unist-util-modify-children'
+import { buildSymbolLinkIndex, generateLinkFromSymbol } from './helpers'
+import { DeclarationReflection } from 'typedoc/dist/lib/models'
+
+interface Options {
+  id?: string
+  typedoc?: DeclarationReflection
+  basePath?: string
+}
+
+type AstTransformer = (tree: any) => void
+
+export default function remarkTypedocSymbolLinks(options: Options = {}): AstTransformer {
+  const typedoc = options.typedoc
+  const basePath = options.basePath || '/'
+
+  const symbolLinkIndex = buildSymbolLinkIndex(typedoc)
+
+  return transformer
+
+  function transformer(tree: any) {
+    const foundLinks = []
+
+    const transformLinks = modifyChildren((node, index, parent) => {
+      if (node.children && node.children.length) {
+        transformLinks(node)
+      }
+
+      if (node.type !== 'linkReference') {
+        return
+      }
+
+      const nodeIndex = parent.children.indexOf(node)
+      const prevNode = parent.children[nodeIndex - 1]
+      const nextNode = parent.children[nodeIndex + 1]
+
+      // A symbol link is interpreted as a linkReference and will
+      // have a left and right text node with `[` and `]` as the last
+      // and first character value, respectively.
+      if (
+        prevNode &&
+        prevNode.type === 'text' &&
+        prevNode.value.endsWith('[') &&
+        nextNode &&
+        nextNode.type === 'text' &&
+        nextNode.value.startsWith(']')
+      ) {
+        const symbol = node.label
+
+        foundLinks.push(symbol)
+
+        // does it have an alias display value? e.g. [[Symbol.method|display text]]
+        const [symbolPath, ...alias] = symbol.split('|')
+        const displayValue = alias.length ? alias.join('') : symbol
+        const symbolLink = generateLinkFromSymbol(symbolPath, basePath, symbolLinkIndex)
+
+        if (process.env.NODE_ENV === 'development' && !symbolLink) {
+          console.warn('remark-typedoc-symbol-links: Could not resolve symbol:', symbol)
+        }
+
+        // replace linkReference with link node
+        const linkNode = {
+          type: 'link',
+          url: symbolLink || '',
+          title: symbolLink
+            ? `View '${symbolPath}' in API reference docs`
+            : "Missing link to symbol in API docs, we're happy to accept a PR to fix this!",
+
+          data: {
+            hProperties: {
+              className: 'symbol',
+              'data-missing': symbolLink ? undefined : true,
+              target: '_blank',
+            },
+          },
+          children: [
+            {
+              type: 'text',
+              value: displayValue,
+            },
+          ],
+        }
+
+        prevNode.value = prevNode.value.substring(0, prevNode.value.length - 1)
+        parent.children.splice(index, 1, linkNode)
+        nextNode.value = nextNode.value.substring(1)
+
+        return index + 1
+      }
+    })
+
+    transformLinks(tree)
+  }
+}
