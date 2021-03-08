6079e62ca833699d355fb2b485e2b5cf2b5563f8
Kamran Ayub <kamran@hey.com>
2021-03-07 21:29:01 -0600

    chore: Add tests for typedoc@0.20.x (#12)

--- example/example.js
+++ example/example.js
@@ -5,7 +5,7 @@ const html = require('remark-html')
 const typedocSymbolLinks = require('../dist')
 
 // Load generated TypeDoc
-const typedoc = JSON.parse(fs.readFileSync('../src/__tests__/typedoc.json'))
+const typedoc = JSON.parse(fs.readFileSync('../src/__tests__/typedoc-0.20.json'))
 
 const doc = unified()
   .use(markdown)
